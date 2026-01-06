fef39a5bfa1ea037ca6bd85b0e1dd64dcbfa9a86
Mohammad Faiz Ullah <mohammadfaizullah123@gmail.com>
2026-01-06 16:53:57 +0530

    scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@ def get_db_connection():
         conn = psycopg2.connect(
             host=os.getenv("DB_HOST"),
             database="postgres",
-            user="postgres",
+            user="postgres.ouqlcjudexkglmxwxtkb",
             password=os.getenv("DB_PASS"),
             port="6543",
             sslmode="require"
@@ -150,6 +150,7 @@ def scrape_gpu_data():
 
 if __name__ == "__main__":
     scrape_gpu_data()
+
 
 
 
